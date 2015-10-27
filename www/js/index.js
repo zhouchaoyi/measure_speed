11f76ebcd21e82004f89e27ef8e7b91072c52073
chou <zhouchaoyi@sohu.com>
2015-10-27 18:00:49 +0800

    1.0版本

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,4 @@
-var TIME=30*1000; //间隔时间
+var TIME=10*1000; //间隔时间
 var HOUR_TIME=TIME/(3600*1000);
 var EARTH_RADIUS = 6378.137;//地球半径
 var prevLocation=null;
@@ -14,6 +14,8 @@ document.addEventListener('deviceready', function () {
             );
         };
     }
+
+
     var noop = function(){}
     window.locationService.getCurrentPosition(function(pos){
         prevLocation={};
@@ -35,15 +37,21 @@ function measureSpeed() {
         var speed=GetSpeed(prevLocation.latitude,prevLocation.longitude,pos.coords.latitude,pos.coords.longitude,HOUR_TIME);
         //alert("prevLocation.latitude="+prevLocation.latitude+"  prevLocation.longitude="+prevLocation.longitude+" pos.latitude="+pos.coords.latitude+" pos.longitude="+pos.coords.longitude+" HOUR_TIME="+HOUR_TIME);
         //alert(speed);
-        var highestSpeed=localStorage.getItem("highest_speed");
-        if(speed>highestSpeed) { 
-            $("#divAlert").css("display","block");
-        }else {
-            $("#divAlert").css("display","none");
-        }
         $("#divCurSpeed").html("当前速度:"+speed+"km/h");
         prevLocation.latitude=pos.coords.latitude;
         prevLocation.longitude=pos.coords.longitude;
+        var highestSpeed=localStorage.getItem("highest_speed");
+        var fileUrl=localStorage.getItem("file_url");
+    
+        if(null!=highestSpeed&&speed>highestSpeed) { 
+            $("#divAlert").css("display","block");
+            if(null!=fileUrl&&fileUrl!="") {
+                playAudio(fileUrl);
+            }
+        }else {
+            $("#divAlert").css("display","none");
+        }
+        
         window.locationService.stop(noop,noop);
     },function(e){
         alert(JSON.stringify(e));
@@ -69,7 +77,7 @@ function GetSpeed(lat1,lng1,lat2,lng2,time)
    s = s * EARTH_RADIUS;
    //s = Math.round(s * 10000) / 10000;
    //return s;
-   var speed=Math.round(s/time * 10000) / 10000;
+   var speed=Math.round(s/time * 100) / 100;
    return speed;
 }
 
@@ -84,6 +92,10 @@ $(document).on("pageinit",function(event){
         $("#divHighSpeed").css("display","block");
         $("#highest_speed").val(highestSpeed);
     }
+    var fileUrl=localStorage.getItem("file_url");
+    if(fileUrl!=null) {
+        $("#urlLabel").html(fileUrl);
+    }
 });
 
 $("#set_speed").click(function(){
@@ -96,11 +108,35 @@ $("#saveBtn").click(function(){
         return;
     }
     localStorage.setItem("highest_speed",$("#highest_speed").val());
+    var fileUrl=$("#audioFile").val();
+    if(null!=fileUrl&&fileUrl!="") {
+        fileUrl="/mnt/sdcard/Recorder/"+fileUrl.split("\\")[2];
+        localStorage.setItem("file_url",fileUrl);
+        $("#urlLabel").html(fileUrl);
+    }
     $("#divHighSpeed").html("设置的最高速度:"+$("#highest_speed").val()+"km/h");
     $("#divHighSpeed").css("display","block");
     $("#divAlert").css("display","none");
     $.mobile.changePage("#index",{transition: "slide"});
 });
+
+
+function playAudio(url) {
+    // Play the audio file at url
+    var my_media = new Media(url,
+        // success callback
+        function () {
+            console.log("playAudio():Audio Success");
+        },
+        // error callback
+        function (err) {
+            console.log("playAudio():Audio Error: " + err);
+        }
+    );
+    // Play audio
+    my_media.play();
+}
+
 
 
 
